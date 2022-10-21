c724e9e73f6479559d5e63734e5e1b638f611a84
juanomanino <juannino@colegioguanenta.edu.co>
2022-10-21 07:20:59 -0500

    Se agregaron ejemplos de listas

--- listas/ejercicios.py
+++ listas/ejercicios.py
@@ -3,12 +3,18 @@
 # Ejercicios manipulación
 
 #1. Consiste en la definición de una lista con elementos de diferentes tipos y el mostrarla por pantalla, tanto entero como por elementos accediendo a la posición que ocupa dentro de la lista.
+print("------Ejercicio 1-------")
+
+
 lista=["Python", "Guanentá", 2022, "Libro", 3]
 print(lista)
 print(lista[0])
 print(lista[2])
 
-# 2. Consiste del uso del operador + para realizar uniones de listas. Además, utilizar la función len para conoceer el número de elementos que componen la lista
+# 2. Consiste del uso del operador + para realizar uniones de listas. Además, utilizar la función len para conoceer el número de elementos que componen la lista.
+print("------Ejercicio 2-------")
+
+
 lista1=["Camiseta", "Pantalón", "Zapatillas"]
 lista2=["Abrigo", "Jersey", "Sudadera", "Calcetines"]
 
@@ -21,7 +27,9 @@ lista_concatenada= lista1+lista2
 print("Número de elementos lista_concatenada: ", len(lista_concatenada))
 print("lista_concatenada: ", lista_concatenada)
 
-# 3. Añadir elementos a la lista de diferentes formas
+# 3. Añadir elementos a la lista de diferentes formas.
+print("------Ejercicio 3-------")
+
 lista=["Camiseta", "Pantalón", "Zapatillas"]
 print(lista)
 lista=lista+["Abrigo"]
@@ -31,11 +39,42 @@ print(lista)
 lista=lista+["Calcetines"]+["Bufanda"]
 print(lista)
 
-# 4. Modificar elementos de una lista y borrar elementos de la misma
+# 4. Modificar elementos de una lista y borrar elementos de la misma.
+print("------Ejercicio 4-------")
 lista=["Camiseta", "Pantalón", "Zapatillas"]
 print(lista)
 lista[1]="Cazadora"
 print(lista)
 del lista[0]
 print(lista)
+
+# 5. Uso del operador * que permite concatenar con ella misma un número finito de veces.
+print("------Ejercicio 5-------")
+
+lista=["Camiseta", "Pantalón", "Zapatillas"]
+print(lista)
+lista_resultante=lista*3
+print(lista_resultante)
+
+# 6. Creación de listas como elementos de listas y acceso a dichos elementos.
+print("------Ejercicio 6-------")
+lista=["Camiseta", ["Calcetines", "Cazadores"], "Zapatillas"]
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[1][0])
+print(lista[1][1])
+
+# 7. Extraer una porción de la lista en una lista nueva.
+print("------Ejercicio 7-------")
+lista=[1,2,3,4,5,6,7,8,9]
+print(lista)
+lista1=lista[3:7]
+print(lista)
+lista2=lista[:5]
+print(lista2)
+lista3=lista[6:]
+print(lista3)
+
 
---
+++ listas/ejercicios2.py
@@ -0,0 +1,49 @@
+# EJERCICIOS LISTAS
+
+# Métodos propios
+
+from os import listxattr
+
+
+lista=[45, 32,3,78]
+print("Lista original: ", lista)
+
+# Función append: añade un elemento a la lista.
+lista.append(995)
+lista.append(7)
+print("lista depués de usar append: ", lista)
+
+# Función sort: ordena la lista.
+lista.sort()
+print("Lista ordenada: ", lista)
+
+# Función reverse:invierte el orden de la lista.
+lista.reverse()
+print("Lista al revés: ", lista)
+
+# Función extend: añade los elementos de una lista a la lista.
+lista_extend=[1,5,87,45]
+lista.extend(lista_extend)
+print("Lista después de extend: ", lista)
+
+# Función count: cuenta el número de veces que aparece el elemento indicado como parámetro dentro de la lista
+print("Número de elementos 45: ", lista.count(45))
+
+# Función insert: añade el elemento pasado como parámetro a la lista en la posición indicada también por parámetro
+lista.insert(4,111)
+print("ista después de insert: ", lista)
+
+# Función remove: elimina la primera ocurrencia empezando por la izquierda de la lista del elemento indicado como parámetro
+lista.remove(45)
+print("Lista después de remove:", lista)
+
+# Función index: devuelve la possición de la primera ocurrencia de izquierda a derecha en la lista del elemento pasado como parámetro.
+print("Posición del elemento 111: ", lista.index(111))
+
+# Función pop: elimina un elemento de la lista y lo devueve comor esultado de la operación.
+lista.pop()
+print("Lista después de pop: ", lista)
+
+# Función clear: elimina todos los elementos de la lista
+lista.clear()
+print("Lista después de clear: ", lista)
